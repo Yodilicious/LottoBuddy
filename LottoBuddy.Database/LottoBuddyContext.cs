ef27fef1146ff89bb92a17c62748acabece3f5de
Jodi Visser <visser.jodi@gmail.com>
2015-10-13 21:23:11 -0400

    Database and Model projects added

---
+++ LottoBuddy.Database/LottoBuddyContext.cs
@@ -0,0 +1,16 @@
+﻿namespace LottoBuddy.Database
+{
+    using Model;
+    using System.Data.Entity;
+
+    public class LottoBuddyContext : DbContext
+    {
+        public LottoBuddyContext()
+            : base("LottoBuddyConnectionString")
+        {
+
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
---
+++ LottoBuddy.Model/Ticket.cs
@@ -0,0 +1,8 @@
+﻿namespace LottoBuddy.Model
+{
+    public class Ticket
+    {
+        public long RecordId { get; set; }
+        public string Note { get; set; }
+    }
+}
